f10810324edc19591cb204d1c6300b9d7695e7dc
KingEvariste <casasjosephm@gmail.com>
2024-12-07 21:04:11 +0800

    feature 2 updated

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const input = document.getElementById("input");
 
 const btn = document.getElementById("submit");
 
+const select = document.getElementById("select");
+
 btn.onclick = ()=>{
   let url = input.value.trim();
   let varURL;
   if (url.length == 59){
     window.open(url);
-  }else{
+  }else if(select.value == 1){
     let i = 0;
     const URLinterval = setInterval(()=>{
       if (i == 9){
@@ -18,5 +20,19 @@ btn.onclick = ()=>{
       window.open(url + `${i}`, `battle ${i}`, `width=500,height=350,left=${left},top=${top}`);
       i++;
     },500)
+  }else if(select.value == 2){
+    let n = 0;
+    const URLinterval2 = setInterval(() => {
+      if (n == 25){
+        clearInterval(URLinterval2);
+      }
+      let chr = String.fromCharCode(97 + n);
+      let top = (n%5) * 180;
+      let left = Math.floor(n/5) * 250;
+      window.open(url + `${chr}`, `battle ${n}`, `width=250,height=180,left=${left},top=${top}`);
+      n++;
+    },700);
+  }else{
+    alert("NOT SUPPORTED YET")
   }
 }
