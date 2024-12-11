b862c80a0493102902af21bf3d4a96bcd4d4110e
KingEvariste <casasjosephm@gmail.com>
2024-12-12 02:40:48 +0800

    fixed but removed force stop feature for now

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
 const input = document.getElementById("input");
 const btn = document.getElementById("submit");
 const select = document.getElementById("select");
-const stop = document.getElementById("stop");
-let URLinterval = null;
+const btn2 = document.getElementById("stopper");
 
-stop.onclick = () => {
-  if(URLinterval != null){
-    clearInterval(URLinterval);
-  }
+let stopped = false;
+
+function stop(){
+  stopped = true;
 }
 
 btn.onclick = ()=>{
   let url = input.value.trim().replace(/ /gi,"");
-  let varURL;
   if (url.length == 59){
     window.open(url);
   }else if(select.value == 1){
     let i = 0;
-    URLinterval = setInterval(()=>{
-      if (i == 9){
+    let URLinterval = setInterval(()=>{
+      if (i == 9 || stopped == true){
         clearInterval(URLinterval);
-        URLinterval = null;
+        stopped = false;
       }
       let top = (i%3) * 350;
       let left = Math.floor(i/3) * 500;
-      window.open(url + `${i}`, `battle ${i}`, `width=500,height=350,left=${left},top=${top}`);
+      window.open(url + `${i}`, `battle ${i}`, `width=500,height=350,left=${left},top=${top},titlebar=no`);
       i++;
-    },100)
+    },300)
   }else if(select.value == 2){
     let n = 0;
-    URLinterval = setInterval(() => {
-      if (n == 25){
-        clearInterval(URLinterval);
-        URLinterval = null;
+    let URLinterval2 = setInterval(() => {
+      if (n == 25 || stopped == true){
+        clearInterval(URLinterval2);
+        stopped = false;
       }
       let chr = String.fromCharCode(97 + n);
       let top = (n%5) * 180;
       let left = Math.floor(n/5) * 250;
-      window.open(url + `${chr}`, `battle ${n}`, `width=250,height=180,left=${left},top=${top}`);
+      window.open(url + `${chr}`, `battle ${n}`, `width=250,height=180,left=${left},top=${top},titlebar=no`);
       n++;
-    },100);
+    },300);
   }else{
     alert("NOT SUPPORTED YET")
   }
