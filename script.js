b4646e2308adb953b732f4bcdeef96c2cf0f3493
KingEvariste <casasjosephm@gmail.com>
2024-12-17 01:49:27 +0800

    fixed tiling

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ btn.onclick = ()=>{
         stopped = false;
       }
       let chr = String.fromCharCode(97 + n);
-      let top = (n%3) * 400;
-      let left = Math.floor(n/3) * 700;
+      let top = (n%2) * 400;
+      let left = Math.floor(n/2) * 700;
       window.open(url + `${chr}`, `battle ${n}`, `width=700,height=400,left=${left},top=${top},titlebar=no`);
       n++;
     },300);
